af37e8a2288c6f78235b84454b22a8dff5734a80
dev2 <juli-felix@outlook.com>
2025-09-18 17:49:35 -0300

    Deixando visual mais limpo. Commit antes de dividir os componentes

---
+++ pdv-frontend/src/app/components/imagem-info/imagem-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagem-info',
+  imports: [],
+  templateUrl: './imagem-info.html',
+  styleUrl: './imagem-info.css'
+})
+export class ImagemInfo {
+
+}
