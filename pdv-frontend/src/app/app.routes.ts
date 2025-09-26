9411771819f78ec799e2ef2db0dddef1f11bc13b
unknown <juli-felix@outlook.com>
2025-09-26 18:08:23 -0300

    Garantindo que esteja 100% atualizado

--- pdv-frontend/src/app/app.routes.ts
+++ pdv-frontend/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { PosComponent } from './components/pos/pos.component';
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 import { ControleDeEstoque } from './components/controle-de-estoque/controle-de-estoque';
-import { ConsultaPreco } from './components/consulta-preco/consulta-preco';
 import { PaginaLogin } from './components/pagina-login/pagina-login';
 
 
